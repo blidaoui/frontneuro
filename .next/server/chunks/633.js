78e9d7f9a62d6583811380f02b97aa8fcb95da3b
blidaoui ibtihel <ibtihel.blidaoui@isimg.tn>
2025-04-17 16:55:56 +0100

    fix

---
+++ .next/server/chunks/633.js
@@ -0,0 +1,95 @@
+exports.id=633,exports.ids=[633],exports.modules={440:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>o});var s=r(1658);let o=async e=>[{type:"image/x-icon",sizes:"16x16",url:(0,s.fillMetadataSegment)(".",await e.params,"favicon.ico")+""}]},593:(e,t,r)=>{"use strict";r.d(t,{default:()=>s});let s=(0,r(2907).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\ASUS\\\\Desktop\\\\test\\\\expnuro\\\\src\\\\app\\\\components\\\\NavBar\\\\index.tsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\ASUS\\Desktop\\test\\expnuro\\src\\app\\components\\NavBar\\index.tsx","default")},647:(e,t,r)=>{Promise.resolve().then(r.t.bind(r,6444,23)),Promise.resolve().then(r.t.bind(r,6042,23)),Promise.resolve().then(r.t.bind(r,8170,23)),Promise.resolve().then(r.t.bind(r,9477,23)),Promise.resolve().then(r.t.bind(r,9345,23)),Promise.resolve().then(r.t.bind(r,2089,23)),Promise.resolve().then(r.t.bind(r,6577,23)),Promise.resolve().then(r.t.bind(r,1307,23))},1135:()=>{},2495:(e,t,r)=>{Promise.resolve().then(r.t.bind(r,6346,23)),Promise.resolve().then(r.t.bind(r,7924,23)),Promise.resolve().then(r.t.bind(r,5656,23)),Promise.resolve().then(r.t.bind(r,99,23)),Promise.resolve().then(r.t.bind(r,8243,23)),Promise.resolve().then(r.t.bind(r,8827,23)),Promise.resolve().then(r.t.bind(r,2763,23)),Promise.resolve().then(r.t.bind(r,7173,23))},3632:()=>{},4431:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>p,metadata:()=>m});var s=r(7413);r(1135);var o=r(593),n=r(5758),a=r(5625),i=r(1194),l=r.n(i),d=r(1267),c=r.n(d);let m={title:{default:"NeuroFlow Consulting",template:"%s | NeuroFlow"},description:"Transformez vos d\xe9fis num\xe9riques en opportunit\xe9s strat\xe9giques",metadataBase:new URL("https://neuroflow-consulting.fr"),openGraph:{images:"/og-image.jpg",locale:"fr_FR",type:"website"}};function p({children:e}){return(0,s.jsx)("html",{lang:"fr",className:`scroll-smooth ${l().variable} ${c().variable}`,children:(0,s.jsxs)("body",{className:"text-gray-100 antialiased",children:[(0,s.jsx)(a.Toaster,{}),(0,s.jsx)(o.default,{}),(0,s.jsx)("main",{id:"main-content",children:e}),(0,s.jsx)(n.default,{})]})})}},5758:(e,t,r)=>{"use strict";r.d(t,{default:()=>s});let s=(0,r(2907).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\ASUS\\\\Desktop\\\\test\\\\expnuro\\\\src\\\\app\\\\components\\\\Common\\\\ScrollUp.tsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\ASUS\\Desktop\\test\\expnuro\\src\\app\\components\\Common\\ScrollUp.tsx","default")},6816:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>s});let s=(0,r(2907).registerClientReference)(function(){throw Error("Attempted to call the default export of \"C:\\\\Users\\\\ASUS\\\\Desktop\\\\test\\\\expnuro\\\\src\\\\app\\\\pages\\\\Contact\\\\page.tsx\" from the server, but it's on the client. It's not possible to invoke a client function from the server, it can only be rendered as a Component or passed to props of a Client Component.")},"C:\\Users\\ASUS\\Desktop\\test\\expnuro\\src\\app\\pages\\Contact\\page.tsx","default")},6933:(e,t,r)=>{Promise.resolve().then(r.bind(r,5625)),Promise.resolve().then(r.bind(r,5758)),Promise.resolve().then(r.bind(r,593))},7809:(e,t,r)=>{"use strict";r.r(t),r.d(t,{default:()=>p});var s=r(687),o=r(3210),n=r(1729),a=r(36),i=r(8222);r(5806);var l=r(7237);let d=(0,a.default)(async()=>{},{loadableGenerated:{modules:["app\\components\\Form.tsx -> @/app/components/ParticleSystem/ParticleSystem"]},ssr:!1,loading:()=>(0,s.jsx)("div",{className:"absolute inset-0 bg-gradient-to-br from-purple-900/30 to-cyan-900/20"})}),c=l.Ay.div`
+  position: relative;
+  width: 100%;
+  max-width: 420px;
+  padding: 20px;
+  background: rgb(229 226 243);
+  backdrop-filter: blur(12px);
+  border-radius: 16px;
+  border: 1px solid rgba(77, 24, 64, 0.1);
+  box-shadow: 0 8px 32px rgba(175, 112, 159, 0.3);
+
+  .form-group {
+  
+    margin-bottom: 45px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  label {
+    margin-bottom: 0.2rem;
+    font-weight: 600;
+    color: #65225d;
+    font-size: 0.95rem;
+  }
+
+  input,
+  textarea {
+    padding: 0.75rem 1rem;
+    border: 1px solid rgb(31 7 23 / 61%);
+    border-radius: 0.5rem;
+    background: rgb(148 84 124 / 9%);
+    color:rgb(64, 14, 64);
+    font-size: 1rem;
+    transition: all 0.3s ease;
+
+    &:focus {
+      outline: none;
+      border-color: rgba(57, 19, 55, 0.5);
+      box-shadow: 0 0 0 3px rgba(167, 139, 250, 0.1);
+    }
+
+    &::placeholder {
+      color:rgb(66, 9, 66);
+    }
+  }
+
+  .form-submit-btn { 
+   margin-bottom:1rem; 
+
+    padding: 1rem;
+    width: 100%;
+    border: none;
+    border-radius: 0.5rem;
+    background: linear-gradient(
+      135deg,
+      rgb(245 111 245) 0%,
+      rgb(53, 14, 59) 100%
+    );
+    color: #ddd6e8;
+    font-size: 1rem;
+    font-weight: 500;
+    cursor: pointer;
+    transition: all 0.3s ease;
+    box-shadow: 0 4px 6px rgba(222, 104, 177, 0.1);
+
+    &:hover {
+      background: linear-gradient(
+        135deg,
+        rgb(182, 98, 181) 0%,
+        rgb(182, 75, 182) 100%
+      );
+      transform: translateY(-2px);
+      box-shadow: 0 6px 12px rgba(0, 0, 0, 0.15);
+    }
+
+    &:disabled {
+      background: linear-gradient(
+        135deg,
+        rgb(190, 181, 204) 0%,
+        rgb(187, 74, 174) 100%
+      );
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+  }
+
+  .form-success-message {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 2rem;
+    text-align: center;
+  }
+`,m=()=>{let[e,t]=(0,o.useState)(!1),[r,a]=(0,o.useState)(""),[l,m]=(0,o.useState)(""),[p,u]=(0,o.useState)(""),[h,x]=(0,o.useState)(""),[f,b]=(0,o.useState)(""),g=async e=>{e.preventDefault();let s=[{value:r,field:"Nom"},{value:l,field:"Pr\xe9nom"},{value:p,field:"Email"},{value:h,field:"T\xe9l\xe9phone"},{value:f,field:"Message"}].find(e=>!e.value.trim());if(s)return i.oR.error(`Le champ '${s.field}' est requis.`);t(!0);try{let e=await fetch("http://localhost:8000/neuroBackend/email/send",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({name:r,last_name:l,number_phone:h,email:p,message:f})});if(!e.ok)throw Error(`HTTP error! status: ${e.status}`);let s=await e.json();console.log("Response data:",s),setTimeout(()=>{t(!1),i.oR.success("Votre message a \xe9t\xe9 envoy\xe9 avec succ\xe8s!"),a(""),m(""),u(""),x(""),b("")},1600)}catch(e){console.error("Submission error:",e),i.oR.error(e instanceof Error?`\xc9chec de l'envoi : ${e.message}`:"Erreur inconnue lors de la soumission"),t(!1)}};return(0,s.jsxs)("div",{className:"relative from-pink-900 to-blue-900 overflow-hidden",children:[(0,s.jsx)(d,{}),(0,s.jsx)(i.N9,{position:"top-center",autoClose:3e3}),(0,s.jsx)(n.P.div,{initial:{opacity:0,y:30},animate:{opacity:1,y:0},transition:{duration:.6,delay:.2},className:"relative flex justify-center",children:(0,s.jsx)(c,{children:e?(0,s.jsxs)("div",{className:"form-success-message",children:[(0,s.jsx)("svg",{className:"w-16 h-16 text-green-400 mx-auto mb-4",fill:"none",stroke:"currentColor",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",children:(0,s.jsx)("path",{strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"2",d:"M5 13l4 4L19 7"})}),(0,s.jsx)("h3",{className:"text-2xl font-bold text-center text-purple-900 mb-2",children:"Message envoy\xe9!"}),(0,s.jsx)("p",{className:"text-center text-purple/80",children:"Nous vous contacterons bient\xf4t."})]}):(0,s.jsxs)("form",{className:"form ",onSubmit:g,children:[(0,s.jsxs)("div",{className:"form-group h-8  ",children:[(0,s.jsx)("label",{htmlFor:"name ",children:"Nom"}),(0,s.jsx)("input",{type:"text",id:"name",name:"name",placeholder:"Entrez votre nom",value:r,onChange:e=>a(e.target.value),required:!0})]}),(0,s.jsxs)("div",{className:"form-group h-8 ",children:[(0,s.jsx)("label",{htmlFor:"last_name",children:"Pr\xe9nom"}),(0,s.jsx)("input",{type:"text",id:"last_name",name:"last_name",placeholder:"Entrez votre pr\xe9nom",value:l,onChange:e=>m(e.target.value),required:!0})]}),(0,s.jsxs)("div",{className:"form-group h-8",children:[(0,s.jsx)("label",{htmlFor:"email",children:"Email"}),(0,s.jsx)("input",{type:"email",id:"email",name:"email",placeholder:"Entrez votre email",value:p,onChange:e=>u(e.target.value),required:!0})]}),(0,s.jsxs)("div",{className:"form-group h-8 ",children:[(0,s.jsx)("label",{htmlFor:"number_phone",children:"T\xe9l\xe9phone"}),(0,s.jsx)("input",{type:"tel",id:"number_phone",name:"number_phone",placeholder:"+1234567890",pattern:"^\\+?[0-9]{7,15}$",value:h,onChange:e=>x(e.target.value),required:!0})]}),(0,s.jsxs)("div",{className:"form-group h-30",children:[(0,s.jsx)("label",{htmlFor:"message",children:"Message"}),(0,s.jsx)("textarea",{id:"message",name:"message",rows:5,placeholder:"D\xe9crivez votre demande",value:f,onChange:e=>b(e.target.value),required:!0})]}),(0,s.jsx)("button",{className:"form-submit-btn",type:"submit",disabled:e,children:e?(0,s.jsxs)("span",{className:"flex items-center justify-center gap-2",children:[(0,s.jsxs)("svg",{className:"animate-spin h-5 w-5 text-white",xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24",children:[(0,s.jsx)("circle",{className:"opacity-25",cx:"12",cy:"12",r:"10",stroke:"currentColor",strokeWidth:"4"}),(0,s.jsx)("path",{className:"opacity-75",fill:"currentColor",d:"M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"})]}),"Envoi en cours..."]}):"Envoyer"})]})})})]})},p=()=>(0,s.jsx)("div",{className:"bg-gradient-to-r from-white via-blue-50 to-purple-100 min-h-screen flex items-center justify-center ",children:(0,s.jsxs)("div",{className:"container mx-auto max-w-7xl grid grid-cols-1 md:grid-cols-2 gap-12 items-center",children:[(0,s.jsxs)(n.P.div,{initial:{opacity:0,x:-40},animate:{opacity:1,x:0},transition:{duration:.6,delay:.2},children:[(0,s.jsx)("div",{className:" text-3xl md:text-5xl font-bold leading-tight mb-6 space-y-6",children:(0,s.jsx)("h2",{className:"bg-gradient-to-r from-purple-400 to-cyan-400 bg-clip-text text-transparent uppercase",children:"Contactez-nous"})}),(0,s.jsx)("p",{className:"text-lg text-gray-900 leading-8 max-w-2xl",children:"Vous avez une question, un projet ou besoin de plus dinformations Remplissez le formulaire et nous vous r\xe9pondrons dans les plus brefs d\xe9lais."})]}),(0,s.jsx)(n.P.div,{initial:{opacity:0,x:40},animate:{opacity:1,x:0},transition:{duration:.6,delay:.3},className:"w-full mb-2 mt-2",children:(0,s.jsx)(m,{})})]})})},8636:(e,t,r)=>{"use strict";function s(){return null}r.d(t,{default:()=>s}),r(3210)},8789:(e,t,r)=>{Promise.resolve().then(r.bind(r,7590)),Promise.resolve().then(r.bind(r,8636)),Promise.resolve().then(r.bind(r,9631))},9631:(e,t,r)=>{"use strict";r.d(t,{default:()=>c});var s=r(687),o=r(1729),n=r(8920),a=r(3210),i=r(1860),l=r(2941),d=r(474);function c(){let[e,t]=(0,a.useState)(!1),[r]=(0,a.useState)(!1),c=[{name:"Accueil",href:"#home"},{name:"Services",href:"#services"},{name:"\xc0 propos",href:"#about"},{name:"Contact",href:"#contact"}],m=(e,r)=>{e.preventDefault();let s=document.getElementById(r);t(!1),s?s.scrollIntoView({behavior:"smooth"}):window.location.hash=r};return(0,s.jsx)(o.P.nav,{initial:{y:0},animate:r?{y:"-100%"}:{y:0},transition:{duration:.3,ease:"easeInOut"},className:"fixed ",children:(0,s.jsxs)("div",{className:"container ",children:[(0,s.jsxs)("div",{className:"flex items-center justify-between  h-20",children:[(0,s.jsx)("a",{href:"#home",onClick:e=>m(e,"home"),children:(0,s.jsxs)("div",{className:"mr-5",children:[" ",(0,s.jsx)(d.default,{src:"/images/logoabout.png",alt:"Logo",width:100,height:100,className:"h-20 w-20",priority:!0})]})}),(0,s.jsx)("div",{className:"hidden md:flex space-x-6",children:c.map(e=>(0,s.jsx)("a",{href:e.href,onClick:t=>m(t,e.href.slice(1)),className:"text-purple-900  font-sans font-medium tracking-wide  dark:text-purple-600 hover:text-pink-300  hover:underline  transition-colors",children:e.name},e.name))}),(0,s.jsx)("button",{className:"md:hidden p-2 items-center justify-center text-dark-purple dark:text-dark-purple",onClick:()=>t(!e),"aria-label":"Menu",children:e?(0,s.jsx)(i.A,{size:24}):(0,s.jsx)(l.A,{size:24})})]}),(0,s.jsx)(n.N,{children:e&&(0,s.jsx)(o.P.div,{initial:{opacity:0,y:-20},animate:{opacity:1,y:0},exit:{opacity:0,y:-20,transition:{duration:.1}},transition:{type:"tween",duration:0},className:"mobile-menu md:hidden dark:bg-custom-color",children:(0,s.jsx)("div",{className:" pb-4 space-y-3",children:c.map(e=>(0,s.jsx)("a",{href:e.href,onClick:t=>m(t,e.href.slice(1)),className:"block text-dark-purple dark:text-dark-purple hover:bg-custom-color dark:hover:text-light-pink px-3 py-2 rounded-md transition-colors",children:e.name},e.name))})},"mobile-menu")})]})})}r(3632)}};
